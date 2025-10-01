c634051999d1aaf879da845277911bbe80e44c4f
gavin-oconnor4 <g.a.oconnor@wustl.edu>
2025-10-01 14:11:36 -0500

    Finished flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,21 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.clear();
+		StdDraw.setXscale(0,18);
+		StdDraw.setYscale(0,12);
+		StdDraw.setPenColor(147,0,76);
+		StdDraw.filledRectangle(9,10,9,2);
+		StdDraw.setPenColor(200,44,76);
+		StdDraw.filledRectangle(9, 2, 9, 2);
+		StdDraw.setPenColor(225, 200, 50);
+		StdDraw.setPenRadius(0.04);
+		StdDraw.arc(9,4.25,4,0,180);
+		StdDraw.setPenColor(102, 51, 0);
+		StdDraw.filledCircle(9,6.5,1);
+		StdDraw.setPenColor(0, 0,0);
+		StdDraw.filledCircle(8.85,6.9,0.15);
+		StdDraw.filledCircle(9.15,7.2,0.15);
 	}
 }
